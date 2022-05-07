cfbb6741a3cdb2ab3852c5e450bf25f2b3be7bc5
Zer0dot <zer0dot.dev@gmail.com>
2022-05-07 19:24:49 -0400

    misc: Added information to readme and comments.

--- src/DotProxy.sol
+++ src/DotProxy.sol
@@ -1,25 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-/**
- * @dev This is the creation code opcode breakdown:
- * 61 eeee:	  	push2 size (which is runtime code size + immutables length)			eeee
- * 80:			dup1																eeee, eeee
- * 60 0a: 		push1 offset (in this case, 10, this is the creation code size)		0a, eeee, eeee
- * 3d:			returndatasize														0, 0a, eeee, eeee
- * 39:			codecopy (stack input: memory loc, offset, size)					eeee
- * 3d:			returndatasize														0, eeee
- * f3:			return (stack input: memory offset, size)							
- */
+
 contract DotProxy {
-	
 	function dotClone(address implementation, bytes32[] calldata immutables) public returns (address) {
 		address instance;
 		assembly {
 			let len := mul(32, immutables.length)
 			// precompute size and reserve 2 bytes for it
-			let runSize := add(45, len) // 45 is base runtime code size, 55 - 10 (creation code)
-			let ptr := mload(64) 		// Get the free memory pointer.
+			let runSize := add(45, len) // 45 is base runtime code size, 55 - 10 (creation code).
+			let ptr := mload(64) // Get the free memory pointer.
 			mstore(ptr, 0x6100000000000000000000000000000000000000000000000000000000000000)
 			mstore(add(ptr, 1), shl(240, runSize))
 			mstore(add(ptr, 3), 0x80600a3d393df3363d3d373d3d3d363d73000000000000000000000000000000)
@@ -31,9 +21,10 @@ contract DotProxy {
 			//		4 bytes for the selector +
 			//		32 bytes for the first parameter (impl address)
 			//		32 bytes for the immutables array calldata location offset
-			//		32 bytes for the immutables array length 
+			//		32 bytes for the immutables array length
 			calldatacopy(add(ptr, 55), 100, len)
 
+			// We create the instance, sending 0 ETH, passing the memory pointer and the full code size.
 			instance := create(0, ptr, add(runSize, 10))
 		}
 		if (instance == address(0)) revert();
