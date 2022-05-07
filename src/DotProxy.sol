dbd209ff67ded62b54faf77e560a9a548963a2fe
Zer0dot <zer0dot.dev@gmail.com>
2022-05-07 18:50:33 -0400

    refactor: Optimization to save like 3 gas again.

--- src/DotProxy.sol
+++ src/DotProxy.sol
@@ -3,14 +3,13 @@ pragma solidity 0.8.13;
 
 /**
  * @dev This is the creation code opcode breakdown:
- * 3d:			returndatasize														0
- * 61 eeee:	  	push2 size (which is runtime code size + immutables length)			eeee, 0
- * 80:			dup1																eeee, eeee, 0
- * 60 0b: 		push1 offset (in this case, 11, this is the creation code size)		0b, eeee, eeee, 0
- * 3d:			returndatasize														0, 0b, eeee, eeee, 0	
- * 39:			codecopy (stack input: memory loc, offset, size)					eeee, 0
- * 3d:			returndatasize														0, eeee, 0
- * f3:			return (stack input: memory offset, size)							0
+ * 61 eeee:	  	push2 size (which is runtime code size + immutables length)			eeee
+ * 80:			dup1																eeee, eeee
+ * 60 0a: 		push1 offset (in this case, 10, this is the creation code size)		0b, eeee, eeee
+ * 3d:			returndatasize														0, 0b, eeee, eeee
+ * 39:			codecopy (stack input: memory loc, offset, size)					eeee
+ * 3d:			returndatasize														0, eeee
+ * f3:			return (stack input: memory offset, size)							
  */
 contract DotProxy {
 	
@@ -19,18 +18,23 @@ contract DotProxy {
 		assembly {
 			let len := mul(32, immutables.length)
 			// precompute size and reserve 2 bytes for it
-			let runSize := add(45, len) // 45 is base runtime code size, 56 - 11 (creation code)
+			let runSize := add(45, len) // 45 is base runtime code size, 55 - 10 (creation code)
 			let ptr := mload(64) 		// Get the free memory pointer.
-			mstore(ptr, 0x3d61000000000000000000000000000000000000000000000000000000000000)
-			mstore(add(ptr, 2), shl(240, runSize))
-			mstore(add(ptr, 4), 0x80600b3d393df3363d3d373d3d3d363d73000000000000000000000000000000)
-			mstore(add(ptr, 21), shl(96, implementation))
-			mstore(add(ptr, 41), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+			mstore(ptr, 0x6100000000000000000000000000000000000000000000000000000000000000)
+			mstore(add(ptr, 1), shl(240, runSize))
+			mstore(add(ptr, 3), 0x80600a3d393df3363d3d373d3d3d363d73000000000000000000000000000000)
+			mstore(add(ptr, 20), shl(96, implementation))
+			mstore(add(ptr, 40), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
 
 			// Copy immutables to memory
-			calldatacopy(add(ptr, 56), 100, len)
+			// 100 is the calldata offset:
+			//		4 bytes for the selector +
+			//		32 bytes for the first parameter (impl address)
+			//		32 bytes for the immutables array calldata location offset
+			//		32 bytes for the immutables array length 
+			calldatacopy(add(ptr, 55), 100, len)
 
-			instance := create(0, ptr, add(runSize, 11))
+			instance := create(0, ptr, add(runSize, 10))
 		}
 		if (instance == address(0)) revert();
 		return instance;
--- test/DotProxy.t.sol
+++ test/DotProxy.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "forge-std/console2.sol";
+// import "forge-std/console2.sol";
 import "../src/DotProxy.sol";
 import "../src/DotImpl.sol";
 
@@ -10,7 +10,7 @@ contract DotProxyTest is Test {
 	DotProxy dotProxy;
 	DotImpl dotImpl;
 
-    uint256 constant MAX_CODE_SIZE = 24576;
+	uint256 constant MAX_CODE_SIZE = 24576;
 	uint256 constant MAX_IMMUTABLE_COUNT = uint256(24576 - 45) / 32;
 	address constant TEST_ADDR = address(1);
 
@@ -28,7 +28,8 @@ contract DotProxyTest is Test {
 			}
 		}
 		DotImpl clone = DotImpl(dotProxy.dotClone(address(dotImpl), addrs));
-		console2.log(address(clone).code.length);
+		
+		assertTrue(address(clone).code.length == 45 + MAX_IMMUTABLE_COUNT * 32);
 		for (uint256 i = 0; i < MAX_IMMUTABLE_COUNT; ) {
 			assertEq(clone.getImmutAtAsAddr(i), TEST_ADDR);
 			unchecked {
@@ -38,14 +39,14 @@ contract DotProxyTest is Test {
 	}
 
 	function testBeyondMaxImmutablesSize() public {
-        bytes32[] memory addrs = new bytes32[](MAX_IMMUTABLE_COUNT + 1);
+		bytes32[] memory addrs = new bytes32[](MAX_IMMUTABLE_COUNT + 1);
 		for (uint256 i = 0; i < MAX_IMMUTABLE_COUNT; ) {
 			addrs[i] = bytes32(uint256(uint160(TEST_ADDR)));
 			unchecked {
 				++i;
 			}
 		}
-        address clone = dotProxy.dotClone(TEST_ADDR, addrs);
-        assertTrue(clone.code.length > 24576);
-    }
+		address clone = dotProxy.dotClone(TEST_ADDR, addrs);
+		assertTrue(clone.code.length > 24576);
+	}
 }
